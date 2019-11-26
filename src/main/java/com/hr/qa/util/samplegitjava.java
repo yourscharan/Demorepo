1b1d62284f25cddfd0ed3bee13401cb3869b3018
yourscharan <57841110+yourscharan@users.noreply.github.com>
2019-11-27 00:28:45 +0530

    Update samplegitjava.java

--- src/main/java/com/hr/qa/util/samplegitjava.java
+++ src/main/java/com/hr/qa/util/samplegitjava.java
@@ -2,7 +2,7 @@ package com.hr.qa.util;
 
 public class samplegitjava {
 	public static void main(String[] args) {
-		System.out.println("sample github");
+		System.out.println("sample github name");
 	}
 
 }
