82934afb3b650e047d8ea2e78042c5a97b3e68d3
MY PC <nagendra3047@gmail.com>
2019-11-27 12:11:27 +0530

    samplegit

--- src/main/java/com/hr/qa/util/samplegitjava.java
+++ src/main/java/com/hr/qa/util/samplegitjava.java
@@ -4,6 +4,8 @@ public class samplegitjava {
 	public static void main(String[] args) {
 		System.out.println("sample github name");
 		System.out.println("sample github name");
+		System.out.println("sample github name");
+
 
 	}
 
